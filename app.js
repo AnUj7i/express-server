4784b867a09567053abd356bc9570d837a2f339c
anuj <anujrocks83@gmail.com>
2025-05-27 15:27:19 +0530

    Initial commit - basic Express server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.get('/intro', (req, res) => {
+  res.send('Hello I am anuj!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
